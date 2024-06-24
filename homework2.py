0f8418c3fc9d07cf9e9b2a3325e6467013a8926f
Tanya-L24 <tanyalebed@inbox.ru>
2024-06-24 22:30:46 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_DZ = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_for_DZ = number_of_hours/number_of_DZ
+print('Курс ', course_name, ', всего задач: ', number_of_DZ, ',', ' затрачено часов: ', number_of_hours, ',', ' среднее время выполнения ', time_for_DZ, ' часа.', sep="")
